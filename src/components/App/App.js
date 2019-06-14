e9a163fa7a64d66c7026089f3b769b18235a8587
Kyle Henderson <kthenderson83@gmail.com>
2019-06-14 13:48:24 -0500

    source in all components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Feeling from '../Feeling/Feeling'
+import Understanding from '../Understanding/Understanding'
+import Support from '../Support/Support'
+import Comments from '../Comments/Comments'
 
 class App extends Component {
   render() {
@@ -14,6 +17,9 @@ class App extends Component {
             <h4><i>Don't forget it!</i></h4>
           </header>
           <Route exact path="/feeling" component={Feeling} />
+          <Route exact path="/understanding" component={Understanding} />
+          <Route exact path="/support" component={Support} />
+          <Route exact path="/comments" component={Comments} />
           <br />
         </div>
       </Router>
