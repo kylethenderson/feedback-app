e8de2efd99cef20129830d8246ac2a84a6042a7c
Kyle Henderson <kthenderson83@gmail.com>
2019-06-14 13:40:45 -0500

    setup redux store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App">        
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stockReducer = (state = [], action) => {
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        stockReducer,
+    }),
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
