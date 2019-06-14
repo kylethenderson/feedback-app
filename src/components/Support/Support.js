4eab8949edac92ca2066a93289dd5e7d0e586ac1
Kyle Henderson <kthenderson83@gmail.com>
2019-06-14 13:45:09 -0500

    update base component structures

---
+++ src/components/Support/Support.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Support extends Component {
+    render () {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default connect()(Support)
