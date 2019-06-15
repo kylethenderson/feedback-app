36ecc6bb20443515b34c88b3aaa11a50c672dace
Kyle Henderson <kthenderson83@gmail.com>
2019-06-15 10:31:04 -0500

    add admin component

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Admin extends Component {
+    render () {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState,
+})
+
+export default connect(mapReduxStateToProps)(Admin)
