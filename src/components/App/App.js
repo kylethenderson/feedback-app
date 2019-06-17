c2c9b20709c85791be117ee442fa4287b4ca43b9
Kyle Henderson <kthenderson83@gmail.com>
2019-06-16 22:22:26 -0500

    update documentation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -40,9 +40,20 @@ class App extends Component {
               <Grid item xs={10} container justify="center">
                 <Route exact path="/admin" component={Admin} />
               </Grid>
+
+              {/* DISCLAIMER REGARDING CONSTANT FEEDBACK REVIEW COMPONENT BELOW*/}
+
+              {/* I wasn't a huge fan of having the feedback displayed throughout the 
+              entire form process, as I felt it detracted from the form process, and not
+              how a normal feedback form might flow.
+              BUT since it was in the readme, I did write the component to do it. 
+              I would prefer this component be commented out or deleted, but again, 
+              since it was in the instructions, I left it in.
+              Comment out the 3 lines below to see form as I preferred          */}
               <Grid item xs={6} >
                 <Route path="/" component={Display} />
               </Grid>
+
             </Grid>
             <br />
           </div>
